aa33968ad7c85c43e265bea74de3742ed0374c29
juliapcp <juliapcp2003@gmail.com>
2022-07-02 12:44:48 -0300

    Atualizado create_sql

--- create.sql
+++ create.sql
@@ -1,29 +1,44 @@
-CREATE DATABASE `sistemaacademico` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
-
-CREATE TABLE `disciplina` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(30) NOT NULL,
-  `cargaHoraria` float NOT NULL,
-  `idProfessor` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `disciplina_FK` (`idProfessor`),
-  CONSTRAINT `disciplina_FK` FOREIGN KEY (`idProfessor`) REFERENCES `professor` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `professor` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- sistemaacademico.aluno definition
 
 CREATE TABLE `aluno` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `matricula` varchar(8) DEFAULT NULL,
   `email` varchar(100) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
+
+
+-- sistemaacademico.professor definition
+
+CREATE TABLE `professor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+
+
+-- sistemaacademico.disciplina definition
+
+CREATE TABLE `disciplina` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(30) NOT NULL,
+  `cargaHoraria` float NOT NULL,
+  `idProfessor` int(11) NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `disciplina_FK` (`idProfessor`),
+  CONSTRAINT `disciplina_FK` FOREIGN KEY (`idProfessor`) REFERENCES `professor` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
+
+
+-- sistemaacademico.alunodisciplina definition
 
 CREATE TABLE `alunodisciplina` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -31,9 +46,11 @@ CREATE TABLE `alunodisciplina` (
   `idDisciplina` int(11) NOT NULL,
   `frequencia` float DEFAULT NULL,
   `media` float DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `alunoDisciplina_un` (`idAluno`,`idDisciplina`),
   KEY `alunodisciplina_FK_1` (`idDisciplina`),
   CONSTRAINT `alunodisciplina_FK` FOREIGN KEY (`idAluno`) REFERENCES `aluno` (`id`),
   CONSTRAINT `alunodisciplina_FK_1` FOREIGN KEY (`idDisciplina`) REFERENCES `disciplina` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
